3cc68b7b8ffa22ee07c200de85afcc70d57e0cdd
Elif Gündüz <elifece3452@gmail.com>
2026-03-03 18:28:50 +0300

    Add files via upload

--- 3.Hafta/App.jsx
+++ 3.Hafta/App.jsx
@@ -1,19 +1,32 @@
+import { useState } from 'react'
 
 import './App.css'
 
-function Deneme({ad}){
-  return(
-    <h2>Merhaba {ad}</h2>
-  );
-
-}
-
 function App(){
+  const [isim,SetIsim]=useState("");
+  const [selamlama,setSelamlama]=useState("");
   return(
     <div>
-      <h1>React İlk Uygulamam</h1>
-      <Deneme ad="Çisem"></Deneme>
-      <Deneme ad="Elif"></Deneme>
+    <h1>3. Hafta React Uygulamam</h1>
+    <input
+     type="text"
+    placeholder="İsminizi Giriniz."
+    value={isim}
+    onChange={(e)=>SetIsim(e.target.value)}
+    />
+    <button onClick={()=>setSelamlama(`Merhaba ${isim}`)}
+    style={{
+      backgroundColor:"red",
+      color:"white",
+      borderRadius:"6px",
+      marginLeft:"20px"
+    }}
+    >Gönder</button>
+    {
+      selamlama && (
+        <p style={{color:"black",fontSize:"25px", fontWeight:"bold"}}>{selamlama}</p>
+      )
+    }
     </div>
   );
 }
